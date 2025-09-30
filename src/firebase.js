6a4664bd191db49618dbaaacc74b09b8cd0dfcf4
Mingzi Yi <mingzi.yee@gmail.com>
2025-09-29 17:38:38 -0700

    Add the home page

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAlLEXb59VKcuOyLlVicF3gZgH9jjdOVfQ",
-  authDomain: "webcafelanding-mingzi.firebaseapp.com",
-  projectId: "webcafelanding-mingzi",
-  storageBucket: "webcafelanding-mingzi.firebasestorage.app",
-  messagingSenderId: "24660014635",
-  appId: "1:24660014635:web:5609079d187852782a5c6e",
-  measurementId: "G-8KTZPTT17N"
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket:REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
